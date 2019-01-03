d516690dbc237015ef80cd60261ecf9b0e3f57d7
blissfish <mark.evertz@gmx.de>
2019-01-03 16:35:08 +0100

    Dependencies updated

--- src/app/projekte/atilla-zoller.component.ts
+++ src/app/projekte/atilla-zoller.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "atillazoller",
-  templateUrl: "./atill-azoller.component.html",
+  templateUrl: "./atilla-zoller.component.html",
   styleUrls: ["./projekte.component.css"]
 })
 export class AtillaZollerComponent {
